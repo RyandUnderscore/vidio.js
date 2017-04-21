ded8f6c87eba6d345307ad76d41d6e2424d1b8c9
RyandUnderscore <ryandunderscore@gmail.com>
2017-04-21 10:32:56 +0700

    Update safelink-3-multi (1).js

--- safelink-3-multi (1).js
+++ safelink-3-multi (1).js
@@ -19,14 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
-if(x=="1"){xxx="http://tipshpsafe.blogspot.com/2017/04/hvordan-velge-riktig-mesothelioma-Law-Firm.html"}
-if(x=="2"){xxx="http://tipshpsafe.blogspot.com/2017/04/see-this-info-for-if-you-want-to-know.html"}
-if(x=="3"){xxx="http://tipshpsafe.blogspot.com/2017/04/the-initial-cause-of-mesothelioma.html"}
-if(x=="4"){xxx="http://tipshpsafe.blogspot.com/2017/04/early-symptoms-of-mesothelioma.html"}
-if(x=="4"){xxx="http://tipshpsafe.blogspot.com/2017/04/diagnosis-of-mesothelioma.html"}
-if(x=="5"){xxx="aweu404ab.html"}a_to_ve[i].href="http://tipshpsafe.blogspot.com/2017/04/mesothelioma-treatment.html"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="mesothelioma-treatment.html"}
+if(x=="2"){xxx="diagnosis-of-mesothelioma.html"}
+if(x=="3"){xxx="early-symptoms-of-mesothelioma.html"}
+if(x=="4"){xxx="the-initial-cause-of-mesothelioma.html"}
+if(x=="5"){xxx="see-this-info-for-if-you-want-to-know.html"}
+if(x=="6"){xxx="hvordan-velge-riktig-mesothelioma-Law-Firm.html"}a_to_ve[i].href="https://tipshpsafe.blogspot.com/2017/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
